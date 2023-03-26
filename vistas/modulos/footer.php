9cff704f13691d0d227e602c8dca381ed4e4a03e
unknown <a3519110233@alumno.uttehuacan.edu.mx>
2023-03-26 13:28:20 -0600

    Sistema Pos

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,10 @@
+
+<footer class="main-footer">
+    <strong>Copyright &copy; 2023 <a href="https://www.facebook.com/people/Tacos-de-cabeza-Don-Cosme/100048562369676/"
+    target="_blank">Tacos de cabeza don Cosme</a>.</strong>
+
+    Todos los derechos reservados.
+
+
+
+</footer>
